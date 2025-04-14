53178224684c4c7c0b5755acf1e36c93724de19e
burkepeterson02 <131825863+burkepeterson02@users.noreply.github.com>
2025-04-14 09:17:52 -0600

    fixed stuff

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/my-app/',
   plugins: [react()],
+  base: '/my-app/', // Replace <REPO_NAME> with your repository name
 })
